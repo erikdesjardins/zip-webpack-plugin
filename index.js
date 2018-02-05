7f5169829dd9bf49f81b1aa42351577bfc0bc267
Steffan Halvorsen <steffanhalv@gmail.com>
2018-02-05 20:10:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ ZipPlugin.prototype.apply = function(compiler) {
         throw new Error('"pathPrefix" must be a relative path');
     }
 
-	compiler.plugin('emit', function(compilation, callback) {
+	compiler.plugin('done', function(compilation, callback) {
 		// assets from child compilers will be included in the parent
 		// so we should not run in child compilers
 		if (this.isChild()) {
